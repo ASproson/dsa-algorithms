0c67c1552ea61a03eea891073b4057fd8e3918b7
atlas <a.sproson@outlook.com>
2024-02-22 11:25:18 +0000

    Removes answer

--- arraysAndStrings/minStartValue.js
+++ arraysAndStrings/minStartValue.js
@@ -1,15 +1,5 @@
 //  Given an array of integers return the minimum positive value such that the step by step sum is never less than 1
 
-function minStartValue(nums) {
-  let ans = 1;
-  let prefixSum = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    prefixSum += nums[i];
-    ans = Math.max(ans, 1 - prefixSum);
-    console.log(ans, prefixSum);
-  }
-  return ans;
-}
+function minStartValue(nums) {}
 
 console.log(minStartValue([-3, 2, -3, 4, 2])); // 5
