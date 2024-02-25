6b660e91c83b9b42ccbc2d7b2ead994b8351fc90
atlas <a.sproson@outlook.com>
2024-02-25 08:52:16 +0000

    Update

---
+++ arraysAndStrings/twoPointers_reverseString.js
@@ -0,0 +1,14 @@
+// Write a function that reverses a string array in place
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// s[i] is a printable ascii character.
+
+function reverseString(s) {}
+
+console.log(reverseString(["h", "e", "l", "l", "o"])); // Output: ["o", "l", "l", "e", "h"]
+console.log(reverseString(["a", "b", "c", "d", "e"])); // Output: ["e", "d", "c", "b", "a"]
+console.log(reverseString(["1", "2", "3", "4", "5"])); // Output: ["5", "4", "3", "2", "1"]
+console.log(reverseString(["apple", "banana", "orange"])); // Output: ["orange", "banana", "apple"]
+console.log(reverseString(["race", "car"])); // Output: ["car", "race"]
+console.log(reverseString([])); // Output: []
+console.log(reverseString(["onlyOne"])); // Output: ["onlyOne"]
