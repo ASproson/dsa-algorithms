9f888a487229aa7276dfae7457466f91c29032d2
atlas <a.sproson@outlook.com>
2024-02-25 08:39:31 +0000

    Rename

---
+++ arraysAndStrings/prefix_minStartValue.js
@@ -0,0 +1,5 @@
+//  Given an array of integers return the minimum positive value such that the step by step sum is never less than 1
+
+function minStartValue(nums) {}
+
+console.log(minStartValue([-3, 2, -3, 4, 2])); // 5
