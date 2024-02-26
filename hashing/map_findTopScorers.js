213bbb9f25b4af439f71057e9ed2be48753645b1
atlas <a.sproson@outlook.com>
2024-02-26 10:33:42 +0000

    Removes answer

--- hashing/map_findTopScorers.js
+++ hashing/map_findTopScorers.js
@@ -1,25 +1,6 @@
 // Given an array of strings that represents a students name and their score on a test, return the top scorers that achieved the highest score on the test
 
-function findTopScorers(students) {
-  const scoreMap = new Map();
-  let maxScore = 0;
-
-  for (const student of students) {
-    const [name, score] = student.split(" ");
-    const parsedScore = parseInt(score);
-
-    if (parsedScore > maxScore) {
-      maxScore = parsedScore;
-    }
-
-    if (!scoreMap.has(parsedScore)) {
-      scoreMap.set(parsedScore, []);
-    }
-    scoreMap.get(parsedScore).push(name);
-  }
-
-  return scoreMap.get(maxScore);
-}
+function findTopScorers(students) {}
 
 console.log(
   findTopScorers([
