53f2b44fc92367a23a128162f8c5511d57b4d358
atlas <a.sproson@outlook.com>
2024-02-26 09:18:05 +0000

    Removes solution

--- arraysAndStrings/fixedSlidingWindow_findMax.js
+++ arraysAndStrings/fixedSlidingWindow_findMax.js
@@ -1,24 +1,5 @@
 // Given an array of integers and a target, find the maximum sum of the integers within any contiguous subarray of the given target
 
-function fixedSlidingWindowFindMax(nums, target) {
-  let left = 0;
-  let current = 0;
+function findMax(nums, target) {}
 
-  for (let i = 0; i < target; i++) {
-    current += nums[i];
-  }
-
-  let answer = current;
-
-  for (let right = target; right < nums.length; right++) {
-    current -= nums[left];
-    current += nums[right];
-    left++;
-
-    answer = Math.max(answer, current);
-  }
-
-  return answer;
-}
-
-console.log(fixedSlidingWindowFindMax([4, 2, 1, 7, 8, 1, 2, 8, 1, 0], 3));
+console.log(findMax([4, 2, 1, 7, 8, 1, 2, 8, 1, 0], 3)); // 16
