95466550609021c31dda8630912e11a9374df9d5
atlas <a.sproson@outlook.com>
2024-02-27 11:21:56 +0000

    Fixes console logs

--- arraysAndStrings/twoPointers_hasSumPair.js
+++ arraysAndStrings/twoPointers_hasSumPair.js
@@ -3,9 +3,9 @@
 function hasSumPair(nums, target) {}
 
 console.log(hasSumPair([1, 2], 3)); // Output: [0, 1] (1 + 2 = 3)
-console.log(hasSumPair([2, 3, 4, 6], 7)); // Output: [0, 2] (2 + 4 = 6)
-console.log(hasSumPair([2, 5, 7, 11, 15], 9)); // Output: [0, 1] (2 + 7 = 9)
+console.log(hasSumPair([2, 3, 4, 6], 7)); // Output: [1, 2] (3 + 4 = 7)
+console.log(hasSumPair([2, 5, 7, 11, 15], 9)); // Output: [0, 2] (2 + 7 = 9)
 console.log(hasSumPair([3, 3], 6)); // Output: [0, 1] (3 + 3 = 6)
-console.log(hasSumPair([-1, 0, 3, 6, 8], 7)); // Output: [2, 4] (3 + 4 = 7)
+console.log(hasSumPair([-1, 0, 3, 6, 8], 7)); // Output: [0, 4] (-1 + 8 = 7)
 console.log(hasSumPair([1, 3, 5, 7], 12)); // Output: [2, 3] (5 + 7 = 12)
 console.log(hasSumPair([1, 3, 5, 7], 9)); // Output: -1 (No pair sums up to 9)
